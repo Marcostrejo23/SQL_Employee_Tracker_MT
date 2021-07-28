58b671e5b74ded4f0f1f3778560b2055d5fbf6a0
Marcos Trejo <marcostrejo23@gmail.com>
2021-07-27 20:03:44 -0700

    pushing role database

--- 02-Homework/Assets/DB/schema.sql
+++ 02-Homework/Assets/DB/schema.sql
@@ -5,5 +5,15 @@ USE employeeTracker_db;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL DEFAULT ""
-)
+    name VARCHAR(30) NOT NULL,
+);
+
+CREATE ROLE (
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    title VARCHAR(30) NOT NULL, 
+    salary DECIMAL NOT NULL, 
+    department_id INT NOT NULL, 
+    FOREIGN KEY (department_id)
+        REFERENCES department(id)
+        ON DELETE CASCADE
+);
