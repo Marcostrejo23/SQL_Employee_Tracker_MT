15a23c800a91da488fe2f05edfe958b17f4990b1
Marcos Trejo <marcostrejo23@gmail.com>
2021-08-30 15:40:06 -0700

    seeds done

---
+++ DB/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (name) VALUES 
+("Legal"),
+("Engineering"),
+("Finance");
+
+INSERT INTO role (title,salary,department_id) VALUES
+("Salesperson",350000,1),
+("Account Manager",100000,2),
+("Lawyer",400000,3),
+("Software Engineer",500000,4),
+("Lead Engineer",100000,5);
+
+INSERT INTO employee (first_name, last_name, role_id,manager_id) VALUES 
+("Marcos","Trejo",1,1),
+("Bob","Dole",2,2),
+("Senior","Frog",3,3),
+("Cat","Dog",4,4);
