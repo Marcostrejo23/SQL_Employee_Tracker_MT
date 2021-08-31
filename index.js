8f353bf866221f5a5f9a4f17f3d9ab83178a5f3a
Marcos Trejo <marcostrejo23@gmail.com>
2021-08-31 09:24:49 -0700

    view roles and departments

--- index.js
+++ index.js
@@ -23,3 +23,27 @@ const departments = () =>{
         }
     })
 };
+
+const employees = () => {
+    db.query("SELECT employees.id, employees.first_name, employees.last_name, employees.manager_id, roles.title, roles.salary, roles.department_id, departments.name FROM employees JOIN roles on employees.role_id=roles.id JOIN departments ON roles.department_id=departments.id", (err,data)=>{
+    if(err){
+        console.log(err)
+        db.end();
+    }else{
+        console.table(data);
+        main();
+    }
+}
+)};
+
+const roles = () => {
+    db.query("SELECT roles.title AS title, salary, departments.name AS department FROM roles JOIN departments ON roles.department_id=departments.id", (err,data)=>{
+        if(err){
+            console.log(err)
+            db.end();
+        }else{
+            console.table(data);
+            main();
+        }
+    }
+    )};
